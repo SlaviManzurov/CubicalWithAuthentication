eab79ed468f48cf1b86e2924266fc79c14a7c65e
SlaviManzurov <77936591+SlaviManzurov@users.noreply.github.com>
2021-02-11 12:53:03 +0200

    Login register form and model for the user

--- config/routes.js
+++ config/routes.js
@@ -2,9 +2,11 @@
 const productController = require('../controllers/productController')
 const aboutController = require('../controllers/aboutController')
 const accerssoryController = require('../controllers/accessoryController')
+const authController = require('../controllers/authController')
 
 module.exports = (app) => {
     app.use('/', productController)
+    app.use('/auth', authController)
     app.use('/about', aboutController)
     app.use('/accessories', accerssoryController)
     app.get('*',(req,res)=>{
